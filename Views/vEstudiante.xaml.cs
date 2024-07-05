5eea58cbc2b072410d5772b8aad195e80773eca4
kpullopaxi <kleverprojas1@gmail.com>
2024-07-05 00:24:07 -0400

    WS

--- Views/vEstudiante.xaml.cs
+++ Views/vEstudiante.xaml.cs
@@ -5,22 +5,37 @@ namespace kpullopaxis6.Views;
 
 public partial class vEstudiante : ContentPage
 {
-	private const string Url = "http://192.168.1.105//semana6/estudiantews.php";
-	private readonly HttpClient cliente= new HttpClient();
-	private ObservableCollection<Models.Estudiante> est;
+    private const string Url = "http://192.168.1.105/semana6/estudiantews.php";
+    private readonly HttpClient cliente = new HttpClient();
+    private ObservableCollection<Models.Estudiante> est;
 
     public vEstudiante()
-	{
-		InitializeComponent();
-		mostrar();
-
+    {
+        InitializeComponent();
+        est = new ObservableCollection<Models.Estudiante>();
+        mostrar();
     }
-	public async void mostrar() 
-	{
-		var content = await cliente.GetStringAsync(Url);	
-		List<Models.Estudiante> mostrar = JsonConvert.DeserializeObject<List<Models.Estudiante>>(content);
-		est = new ObservableCollection<Models.Estudiante>(mostrar);
-        ListaEstudiantes.ItemsSource = est;
 
-	}
+    public async void mostrar()
+    {
+        try
+        {
+            var content = await cliente.GetStringAsync(Url);
+            var mostrar = JsonConvert.DeserializeObject<List<Models.Estudiante>>(content);
+            est = new ObservableCollection<Models.Estudiante>(mostrar);
+            ListaEstudiantes.ItemsSource = est;
+        }
+        catch (JsonReaderException jsonEx)
+        {
+            await DisplayAlert("Error", "Error al deserializar el JSON: " + jsonEx.Message, "OK");
+        }
+        catch (HttpRequestException httpEx)
+        {
+            await DisplayAlert("Error", "Error en la solicitud HTTP: " + httpEx.Message, "OK");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", "Error inesperado: " + ex.Message, "OK");
+        }
+    }
 }
